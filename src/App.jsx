25bb7c53ef7c6d8a364e80472de4b4975ed8a027
MayankBelwal05 <mbelwal05@gmail.com>
2025-05-16 10:01:23 +0530

    Updated : Added check session, Protected Route

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import OtpVerify from './components/OtpVerify';
-import OnboardingForm from './components/steps/OnboardingForm';
+import OnboardingForm from './components/OnboardingForm';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<OtpVerify />} />
-        <Route path="/onboarding" element={<OnboardingForm />} />
-      </Routes>
-    </Router>
+    <div className="position-relative min-vh-100" style={{ backgroundColor: '#d4fcdc' }}>
+      <img src="/mnivesh-logo.png" alt="mNivesh Logo" className="position-absolute"
+        style={{top: '10px', left: '20px',width: '160px',height: 'auto',zIndex: 999,
+        }} />
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<OtpVerify />} />
+          <Route path="/onboarding" element={ <ProtectedRoute> <OnboardingForm /> </ProtectedRoute> }/>
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
