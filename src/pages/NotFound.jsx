28ad1f4437c239102f87820a1cad526988bf9427
Aryan Chauhan <rajaryagautam@gmail.com>
2025-08-08 18:31:53 +0530

    404 added

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,15 @@
+// src/pages/NotFound.jsx
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className="min-vh-100 d-flex align-items-center justify-content-center">
+      <div className="text-center">
+        <h1 className="display-4">404</h1>
+        <p className="lead">Page not found.</p>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
